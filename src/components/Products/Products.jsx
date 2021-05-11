1b64585c134424fda7ae09858300fdb9fe7fb6b5
xoonziip <sanjivchaudhary416@gmail.com>
2021-05-11 23:43:34 +0545

    bug fixes

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import Grid from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
-import Product from './product/Product'
+import Product from './Product/Product';
 
-const product =[
+const products =[
     {id:1, name:'shoes', description: 'Running shoes.', price:'$5'},
     {id:2, name:'t-shirt', description: 'male t-shirt.', price:'$10' },
 
 ]
 
-const Product = () =>{
-    <main>
+const Products = () =>{
+    return(
+
+        <main>
         <Grid container justify="center" spacing={4}>
-            {product.map((product) =>(
+            {products.map((product) =>(
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <product product={product}/>
+                    <Product product={product}/>
                 </Grid>
             ))}
 
         </Grid>
     </main>
 
+    );
+
 }
 
-export default Product;
+export default Products;
