cd80e4efac85c5a765146f63ea1790db4cfc3cf5
xoonziip <sanjivchaudhary416@gmail.com>
2021-05-12 14:36:18 +0545

    error fixes

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Container, Typography, Button, Grid } from '@material-ui/core';
-import classes from '*.module.css';
+import useStyles from './style';
 
 const Cart = ({cart}) => {
-    const isEmpty= !cart.line_items.length;
+    const classes = useStyles();
+
     const EmptyCart= () =>(
         <Typography variant="subtitle1">You have no Items in your shopping Cart, start adding Some!</Typography>
     );
@@ -29,13 +30,15 @@ const Cart = ({cart}) => {
 
             </div>
         </>
-    )
+    );
+
+    if(!cart.line_items) return 'loading....';
  
     return (
         <Container>
             <div className={classes.toolbar}/>
             <Typography className={classes.title} variant="h3"> Your Shopping Cart</Typography>
-            {isEmpty ? <EmptyCart/> : <FilledCart/>}
+            {!cart.line_items.length ? <EmptyCart/> : <FilledCart/>}
         </Container>
     )
 }
