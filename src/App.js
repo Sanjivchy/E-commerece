bcc7fea741860d500420c2d39015156927fed5f7
xoonziip <sanjivchaudhary416@gmail.com>
2021-05-12 13:40:05 +0545

    added cart button and cart items number

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import { commerce } from './lib/commerce';
 import {Products, Navbar } from './components';
 const App = () => {
     const [products, setProducts] =useState([]);
+    const [cart, setCart] = useState({});
 
+// fetching products
     const fetchProducts = async () => {
         const {data} = await commerce.products.list();
         setProducts(data);
     }
+// fetching cart
+    const fetchCart = async () => {
+       setCart(await commerce.cart.retrieve()); 
+    }
+
+// function for add product to cart
+const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+
+}
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     },[]);
-    console.log(products);
 
+    console.log(cart);
     return (
         <div>
-            <Navbar/>
-           <Products products={products}/>
+            <Navbar totalItems={cart.total_items}/>
+           <Products products={products} onAddToCart={handleAddToCart}/>
 
         </div>
     )
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -5,7 +5,7 @@ import {AddShoppingCart} from '@material-ui/icons';
 import useStyles from './style';
 
 
-const Product = ({product}) => {
+const Product = ({product, onAddToCart}) => {
     const classes = useStyles();
 
    
@@ -18,17 +18,22 @@ const Product = ({product}) => {
                    <Typography variant="h5" gutterBottom>
                        {product.name}
                    </Typography>
+
                    <Typography variant="h5" gutterBottom>
                        {product.price.formatted_with_symbol}
                    </Typography>
+
                 </div>
+
                 <Typography dangerouslySetInnerHTML={{__html:product.description}} variant="body2" color="textSecondary"/>
            </CardContent>
+
            <CardActions disableSpacing className={classes.CardActions}>
-               <IconButton aria-label="Add to Cart">
+               <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
                     <AddShoppingCart/>
                </IconButton>
            </CardActions>
+
        </Card>
     )
 }
