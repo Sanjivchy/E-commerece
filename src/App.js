c6ff3abefc01e82b7e54df3f207ba737c7291cf5
xoonziip <sanjivchaudhary416@gmail.com>
2021-05-12 23:35:57 +0545

    added cart buttons

--- src/App.js
+++ src/App.js
@@ -20,9 +20,24 @@ const App = () => {
 
 // function for add product to cart
 const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
 
+}
+
+const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, {quantity});
+    setCart(cart)
+}
+
+const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+}
+
+const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
 }
 
     useEffect(() => {
@@ -40,7 +55,12 @@ const handleAddToCart = async (productId, quantity) => {
                         <Products products={products} onAddToCart={handleAddToCart}/>
                     </Route>
                     <Route exact path='/Cart'>
-                        <Cart cart={cart}/>
+                        <Cart 
+                            cart={cart}
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart = {handleRemoveFromCart}
+                            handleEmptyCart ={handleEmptyCart}
+                        />
                     </Route>
                 </Switch>         
              </div>
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -3,7 +3,7 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@
 
 import useStyles from './style';
 
-const CartItem = ({item}) => {
+const CartItem = ({item ,onUpdateCartQty, onRemoveFromCart }) => {
     const classes = useStyles();
     return (
         <div>
@@ -12,13 +12,14 @@ const CartItem = ({item}) => {
                 <Typography variant="h4">{item.name}</Typography>
                 <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
            </CardContent>
+
             <CardActions className={classes.cartActions}>
                 <div className={classes.buttons}>
-                    <Button type="button" size="small">-</Button>
+                    <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type="button" size="small">+</Button>
+                    <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
                 </div>
-                <Button variant="contained" type="button" color="secondary">Remove</Button>
+                <Button variant="contained" type="button" color="secondary" onClick={() => onRemoveFromCart(item.id)}>Remove</Button>
             </CardActions>
         </div>
     )
